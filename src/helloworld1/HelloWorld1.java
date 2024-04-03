cba625043e86385157b0139e4d6b0048406e458c
Chris <foreverheld01@gmail.com>
2024-04-03 14:22:27 +0200

    my first commit

---
+++ src/helloworld1/HelloWorld1.java
@@ -0,0 +1,12 @@
+package helloworld1;
+
+import javax.swing.JOptionPane;
+
+public class HelloWorld1 {
+
+    public static void main(String[] args) 
+    {
+     JOptionPane.showMessageDialog(null, "Hello World");
+    }
+
+}
